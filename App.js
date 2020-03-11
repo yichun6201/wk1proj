73ea19ae90046a939fefe17aff97a0fe9c80b889
yichun6201 <39977904+yichun6201@users.noreply.github.com>
2020-03-12 02:52:47 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>110619022</Text>
+      <Text>李依純</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
